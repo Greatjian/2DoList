d2ab628410c44fa097e9bf3cb367aeec00413fc4
Greatjian <jefffreywen94@gmail.com>
2017-09-10 21:19:52 -0400

    first commit

---
+++ app/src/main/java/com/onemonth/todolist/Constants.java
@@ -0,0 +1,11 @@
+package com.onemonth.todolist;
+
+/**
+ * Created by Greatjian on 9/1/17.
+ */
+public class Constants
+{
+    public static final int ADD_ITEM_REQUEST_CODE = 1;
+
+    public static final String ADD_ITEM_RESULT_KEY = "add_item_result";
+}
